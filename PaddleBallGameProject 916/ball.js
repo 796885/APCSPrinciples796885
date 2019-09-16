1d5e23613133188d88b25ff1e77430a468f8c21a
796885 <796885@seq.org>
2019-09-16 08:51:14 -0700

    PaddleBall Game

---
+++ PaddleBallGameProject 916/ball.js
@@ -0,0 +1,61 @@
+class Ball {
+  constructor(x, y, dx, dy, id){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0,0.5,);
+    this.clr = color(random(255), random(255), random(255));
+    // this.w=25
+  }
+
+  run(){
+    this.checkedges();
+    this.update();
+    this.render();
+    this.Bounce();
+  }
+
+  checkedges(){
+    if(this.loc.x < 0){
+      this.vel.x= -this.vel.x
+    }
+    if(this.loc.x> width){
+      this.vel.x= -this.vel.x
+    }
+    if(this.loc.y < 0){
+      this.vel.y= -this.vel.y
+    }
+    if(this.loc.y> height){
+      this.vel.y= -this.vel.y
+    }
+  }
+  update(){
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+  }
+
+  render(){
+
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y,20,20);
+  }
+
+
+  IsColliding(){
+    if(this.loc.x > paddle.loc.x &&
+      this.loc.x < paddle.loc.x + paddle.w &&
+      this.loc.y > paddle.loc.y &&
+      this.loc.y < paddle.loc.y + paddle.h){
+        return true;
+      } else{
+        return false;
+      }
+    }
+
+
+    Bounce(){
+      if (this.IsColliding()=== true){
+        this.vel.x = -this.vel.x;
+        this.vel.y= - this.vel.y;
+      }
+    }
+}
