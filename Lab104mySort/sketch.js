80044e8788633199e70f780bfd86b65c4ad5ff6b
796885 <796885@seq.org>
2019-10-04 08:50:54 -0700

    mySort10/4

---
+++ Lab104mySort/sketch.js
@@ -0,0 +1,24 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+var list = [1,4,786,54,12,96]
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
+function swap(list, a, b){
+  var temp = list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
+function natSort
