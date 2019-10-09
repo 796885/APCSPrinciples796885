420ed08730b95f8a7f94643a6a5cb9fab4ee3b9b
796885 <796885@seq.org>
2019-10-08 23:30:14 -0700

    checkerboard1008

--- Lab1007CheckerBaord/StartCode/CheckerBoard/square.js
+++ Lab1007CheckerBaord/StartCode/CheckerBoard/square.js
@@ -1,10 +1,9 @@
 class Square {
   constructor(x, y, w, h){
-    this.loc = createVector(x, y);
-    this.w=w;
-    this.h= h;
-    this.clr = color(random(255), random(255), random(255));
-    this.w=200
+    this.x = 0;
+    this.y=0;
+    this.w= 100;
+    this.h = 100;
 //  Add perameters to your constructor function
 function Sqr(){
  if
