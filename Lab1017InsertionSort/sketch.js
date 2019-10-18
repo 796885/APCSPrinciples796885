2e44450015ccbed51e2b2aa3cfc297b869b2b7ae
796885 <796885@seq.org>
2019-10-17 21:45:53 -0700

    InsertionSort 10/17

--- Lab1017InsertionSort/sketch.js
+++ Lab1017InsertionSort/sketch.js
@@ -3,6 +3,7 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var list
+for Random(list>0, list<10,000)
 InsertionSort()
 
 function setup() {
@@ -19,7 +20,18 @@ function draw() {
 
 }
 function InsertionSort();
-  for var( )
+ insertionSort = (nums) => {
+for (let i = 1; i < nums.length; i++) {
+  let j = i - 1
+  let tmp = nums[i]
+  while (j >= 0 && nums[j] > tmp) {
+    nums[j + 1] = nums[j]
+    j--
+  }
+  nums[j+1] = tmp
+}
+return nums
+}
 
 swap(list,i,j){
   var temp = list[i];
