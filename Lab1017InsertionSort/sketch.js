8553988a3ca50893ebc697fc8fa916b94750bf7a
796885 <796885@seq.org>
2019-10-18 08:48:50 -0700

    InsertionSort10/18
    finished

--- Lab1017InsertionSort/sketch.js
+++ Lab1017InsertionSort/sketch.js
@@ -2,9 +2,7 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var list
-for Random(list>0, list<10,000)
-InsertionSort()
+var list = []
 
 function setup() {
   var cnv = createCanvas(800, 800);
@@ -12,6 +10,9 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
 
+    for (var x = 0; x < 10; x ++){
+        list[x] = Math.floor(random(1,100));
+      }
 
 }
 
@@ -19,22 +20,19 @@ function setup() {
 function draw() {
 
 }
-function InsertionSort();
- insertionSort = (nums) => {
-for (let i = 1; i < nums.length; i++) {
-  let j = i - 1
-  let tmp = nums[i]
-  while (j >= 0 && nums[j] > tmp) {
-    nums[j + 1] = nums[j]
-    j--
-  }
-  nums[j+1] = tmp
-}
-return nums
-}
 
-swap(list,i,j){
-  var temp = list[i];
-      list = list[j]
-      list = temp;
+function InsertionSort(list){
+ for (var i = 1; i < list.length; i++){
+   for (var j = i; j > 0; j--){
+     if(list[j] < list[j-1]){
+       swap (list, j, j-1);
+     }
+   }
+ }
+}
+function swap (list, x, y){
+  var tmp = list[x];
+  list[x] = list[y];
+  list[y] = tmp;
+
 }
