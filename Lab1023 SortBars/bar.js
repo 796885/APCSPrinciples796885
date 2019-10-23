9c7f819d2c7ba26efc445da38c48040ff1054184
796885 <796885@seq.org>
2019-10-23 08:48:00 -0700

    10/23SortBars

---
+++ Lab1023 SortBars/bar.js
@@ -0,0 +1,7 @@
+class Bar {
+  constructor(x, y, w, h){
+    this.loc = createVector(x, y);
+    this.w=w;
+    this.h= h;
+    this.clr = color(random(255), random(255), random(255));
+    this.w=200
---
+++ Lab1023 SortBars/sketch.js
@@ -0,0 +1,22 @@
+//  Natalie Hallmann
+// 	10/23 SortBars
+//  This is a comment
+//  The setup function function is called once when your program begins
+var bars=[];
+
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+function loadBars();
+  for (var i = 0)
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
