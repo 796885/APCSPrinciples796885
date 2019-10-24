bb476556bf9d1540b43136570500ef0d213c799a
796885 <796885@seq.org>
2019-10-24 08:49:03 -0700

    SortBars10/24
    working on it

--- Lab1023 SortBars/bar.js
+++ Lab1023 SortBars/bar.js
@@ -1,7 +1,11 @@
 class Bar {
-  constructor(x, y, w, h){
-    this.loc = createVector(x, y);
-    this.w=w;
-    this.h= h;
-    this.clr = color(random(255), random(255), random(255));
-    this.w=200
+  constructor(location){
+    this.loc = location;
+    this.w= barWidth;
+    this.h= barheight - this.loc.y
+
+  }
+  render(){
+    fill(202,50,100);
+    rect(this.loc.x, this.loc.y, this.w, this.h)
+  }
--- Lab1023 SortBars/sketch.js
+++ Lab1023 SortBars/sketch.js
@@ -5,18 +5,50 @@
 var bars=[];
 
 
+loadBars(bars);
+runBars();
+bubbleSort();
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
 
-
+  for (var x = 0; x < 10; x++){
+    bars[x] = Math.floor(random(1,100));
+  }
 }
-function loadBars();
-  for (var i = 0)
-
+function loadBars(num){
+  for (var i = 0; i < num; i++){
+    var barHeight = Math.florr(random(height));
+    var loc = createVector(i*barWidth, barHeight);
+    bars[i] = new Bar(loc);
+  }
+}
 //  The draw function is called @ 30 fps
-function draw() {
+function draw(){
+  background(20, 20,100);
+  frameRate(1);
+  runBars();
 
 }
+function runBars(){
+  for(var i = 0; i < bars.length; i++){
+    bars[i].render()
+  }
+}
+
+function bubbleSort(list){
+  for(var j = bars.length; j > 0 ; j --){
+      for (var i = 0 ; i < bars.length; i ++){
+
+          if (bars[i+1] < bars [i]){
+            var temp = bars[i];
+            bars[i] = bars[i+1];
+            bars[i+1] = temp;
+          }
+        }
+      }
+      console.log(list);
+    }
