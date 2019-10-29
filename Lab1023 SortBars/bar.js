352b42e5e88da34b95a9f9b1feb66e1bb96f8e62
796885 <796885@seq.org>
2019-10-29 08:50:31 -0700

    sortBars 10/29

--- Lab1023 SortBars/bar.js
+++ Lab1023 SortBars/bar.js
@@ -1,16 +1,20 @@
 class Bar {
-  constructor(location){
-    this.loc = location;
-    this.w= barWidth;
-    this.h= barheight - this.loc.y;
+  constructor(x,y,w,h,c) {
+    this.loc = createVector(x,y);
+    this.w= w;
+    this.h= h;
 
   }
   render(){
     fill(202,50,100);
     rect(this.loc.x, this.loc.y, this.w, this.h);
   }
-}
+
 
 run(){
-  this.render;
+  this.render();
+}
+set(i){
+  this.loc.x=i*barWidth;
+}
 }
--- Lab1023 SortBars/sketch.js
+++ Lab1023 SortBars/sketch.js
@@ -3,57 +3,59 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var bars=[];
+var barWidth = 30;
 
 
-loadBars(bars);
-runBars();
-bubbleSort();
+
 
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  background(200, 30, 150);
 
-  for (var x = 0; x < 10; x++){
-    bars[x] = Math.floor(random(1,100));
+  loadBars(bars);
+frameRate(15);
+
+  for (var i = 0; i < bars.length; i++){
+    bars[i].run();
   }
 }
-function loadBars(num){
-  for (var i = 0; i < num; i++){
-    var barHeight = Math.floor(random(height));
-    var loc = createVector(i*barWidth, barHeight);
-    bars[i] = new Bar(loc);
-  }
-}
+
 //  The draw function is called @ 30 fps
 function draw(){
-  background(20, 20,100);
-  frameRate(1);
-  runBars();
-
+  bubbleSort();
 }
+
+function loadBars(){
+  for (var x=0; x<60; x++){
+    var w=int(random(1,60));
+    bars[x]= new Bar(barWidth*x,800-(barWidth*w),barWidth,(barWidth*w))
+  }
+}
+
+
 function update(){
   for(var i=0; i<bars.length; i++){
     bars[i].set(i);
   }
-}
-function runBars(){
-  for(var i = 0; i < bars.length; i++){
-    bars[i].render();
+  background(200, 30, 150);
+  for(var i=0; i<bars.length; i++){
+    bars[i].run();
   }
 }
 
 function bubbleSort(bars){
-  for(var j = bars.length; j > 0 ; j --){
-      for (var i = 0 ; i < bars.length; i ++){
-
-          if (bars[i+1] < bars [i]){
-            var temp = bars[i];
-            bars[i] = bars[i+1];
-            bars[i+1] = temp;
-          }
+  for(var j =0; j > bars.length-1; j++){
+      if (bars[j].h>bars[j+1].h){
+        swap(bars, j, j+1);
+        update();
+      }
         }
       }
-      console.log(bars);
-    }
+
+
+function swap(bars,i,j){
+  var temp = bars[i];
+  bars[i] = bars[j];
+  bars[j] = temp;
+}
