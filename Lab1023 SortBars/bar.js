520767ede233f8e93f6f3c4109f3cf776c35c1de
796885 <796885@seq.org>
2019-10-30 07:47:34 -0700

    sortBars 10/30
    finished

--- Lab1023 SortBars/bar.js
+++ Lab1023 SortBars/bar.js
@@ -3,10 +3,10 @@ class Bar {
     this.loc = createVector(x,y);
     this.w= w;
     this.h= h;
-
+    this.clr=c;
   }
   render(){
-    fill(202,50,100);
+    fill(this.clr);
     rect(this.loc.x, this.loc.y, this.w, this.h);
   }
 
--- Lab1023 SortBars/sketch.js
+++ Lab1023 SortBars/sketch.js
@@ -11,7 +11,7 @@ var barWidth = 30;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(200, 30, 150);
+  background(54, 79, 63);
 
   loadBars(bars);
 frameRate(15);
@@ -27,9 +27,9 @@ function draw(){
 }
 
 function loadBars(){
-  for (var x=0; x<60; x++){
-    var w=int(random(1,60));
-    bars[x]= new Bar(barWidth*x,800-(barWidth*w),barWidth,(barWidth*w))
+  for (var x=0; x<30; x++){
+    var w=int(random(1,30));
+    bars[x]= new Bar(barWidth*x,800-(barWidth*w),barWidth,(barWidth*w), color(72, 96, 115));
   }
 }
 
@@ -38,14 +38,14 @@ function update(){
   for(var i=0; i<bars.length; i++){
     bars[i].set(i);
   }
-  background(200, 30, 150);
+  background(54, 79, 63);
   for(var i=0; i<bars.length; i++){
     bars[i].run();
   }
 }
 
-function bubbleSort(bars){
-  for(var j =0; j > bars.length-1; j++){
+function bubbleSort(){
+  for(var j =0; j < bars.length -1; j++){
       if (bars[j].h>bars[j+1].h){
         swap(bars, j, j+1);
         update();
